8e43467c5add5f4f9599a097cae7a783008ae011
agung <agung@biido.id>
2018-12-30 08:29:53 +0700

    asdfasdf

--- cnn.py
+++ cnn.py
@@ -92,7 +92,9 @@ class ConvLayer(object):
                 # log.debug("e : %s", type(f))
 
 
-                self.z_values[j][i] = np.add(np.sum(np.multiply(input_neurons[:, row:self.filter_size + row, slide:self.filter_size + slide], self.weights[j])), self.biases[j])
+                # self.z_values[j][i] = np.add(np.sum(np.multiply(input_neurons[:, row:self.filter_size + row, slide:self.filter_size + slide], self.weights[j])), self.biases[j])
+
+                self.z_values[j][i] = np.sum(input_neurons[:, row:self.filter_size + row, slide:self.filter_size + slide] * self.weights[j]) + self.biases[j]
                 self.output[j][i] = activation(self.z_values[j][i])  # activation function
 
                 # print "input_neurons sub : ",input_neurons[:, row:self.filter_size + row, slide:self.filter_size + slide].shape
