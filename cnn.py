d1e9324d0c359d2ba0091424f2e3bae6c41ca6b0
agung <agung@biido.id>
2018-12-31 16:53:35 +0700

    asdfasdf

--- cnn.py
+++ cnn.py
@@ -76,32 +76,32 @@ class ConvLayer(object):
 
                 # ACTIVATIONS -> loop through each conv block horizontally
 
-                a = input_neurons[:, row:self.filter_size + row, slide:self.filter_size + slide]
-                b = self.weights[j]
-                c = self.biases[j]
-                log.debug("a : %s", a.shape)
-                log.debug("b : %s", b.shape)
-                log.debug("c : %s", c.shape)
-
-                d = np.multiply(a,b)
-                e = np.sum(d)
-                log.debug("d : %s", d.shape)
-                log.debug("e : %s", e.shape)
-
-                f = np.add(e,c)
-                log.debug("f : %s", type(f))
-                log.debug("f : %s", f)
-                log.debug("self.z_values[j][i].shape : %s", type(self.z_values[j][i]))
+                # a = input_neurons[:, row:self.filter_size + row, slide:self.filter_size + slide]
+                # b = self.weights[j]
+                # c = self.biases[j]
+                # log.debug("a : %s", a.shape)
+                # log.debug("b : %s", b.shape)
+                # log.debug("c : %s", c.shape)
+                #
+                # d = np.multiply(a,b)
+                # e = np.sum(d)
+                # log.debug("d : %s", d.shape)
+                # log.debug("e : %s", e.shape)
+                #
+                # f = np.add(e,c)
+                # log.debug("f : %s", type(f))
+                # log.debug("f : %s", f)
+                # log.debug("self.z_values[j][i].shape : %s", type(self.z_values[j][i]))
 
                 # self.z_values[j][i] = np.add(np.sum(np.multiply(input_neurons[:, row:self.filter_size + row, slide:self.filter_size + slide], self.weights[j])), self.biases[j])
                 self.z_values[j][i] = float(np.sum(input_neurons[:, row:self.filter_size + row, slide:self.filter_size + slide] * self.weights[j]) + self.biases[j])
 
                 # self.z_values[j][i] = f
-                log.debug("self.z_values[j][i].shape : %s", self.z_values[j][i])
-                log.debug("self.z_values[j][i].shape : %s", type(self.z_values[j][i]))
+                # log.debug("self.z_values[j][i].shape : %s", self.z_values[j][i])
+                # log.debug("self.z_values[j][i].shape : %s", type(self.z_values[j][i]))
                 self.output[j][i] = activation(self.z_values[j][i])  # activation function
 
-                sys.exit(0)
+                # sys.exit(0)
 
                 # print "input_neurons sub : ",input_neurons[:, row:self.filter_size + row, slide:self.filter_size + slide].shape
                 # print "self.weights[j].shape : ",self.weights[j].shape
