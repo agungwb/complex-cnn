bf621c4f4a9f06247580a18fc1b47720de040231
agung <agung@biido.id>
2019-01-03 10:13:56 +0700

    asdf

---
+++ test/sort.py
@@ -0,0 +1,33 @@
+import numba
+#from numba import cuda
+def bubblesort(X):
+    N = len(X)
+    for end in range(N, 1, -1):
+        for i in range(end - 1):
+            cur = X[i]
+            if cur > X[i + 1]:
+                tmp = X[i]
+                X[i] = X[i + 1]
+                X[i + 1] = tmp
+
+@numba.jit(nopython=True, parallel=True)
+def bubblesort_jit(X):
+    N = len(X)
+    for end in range(N, 1, -1):
+        for i in range(end - 1):
+            cur = X[i]
+            if cur > X[i + 1]:
+                tmp = X[i]
+                X[i] = X[i + 1]
+                X[i + 1] = tmp
+
+#@cuda.jit('void(int32[:])')
+def bubblesort_cuda(X):
+    N = len(X)
+    for end in range(N, 1, -1):
+        for i in range(end - 1):
+            cur = X[i]
+            if cur > X[i + 1]:
+                tmp = X[i]
+                X[i] = X[i + 1]
+                X[i + 1] = tmp
